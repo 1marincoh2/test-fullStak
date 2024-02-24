d119946f911356e38b5785098a2f0c3638c1d91c
1marincoh2 <josealbertomarincoh1@gmail.com>
2024-02-24 15:40:06 -0500

    feat:modulo 1 finalizado

--- front_test_pokeApi/src/page/PokemonList.tsx
+++ front_test_pokeApi/src/page/PokemonList.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
@@ -8,18 +8,18 @@ import Typography from "@mui/material/Typography";
 import Pagination from "@mui/material/Pagination";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
 import LinearProgress from "@mui/material/LinearProgress";
 import Skeleton from "@mui/material/Skeleton";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import Button from "@mui/material/Button";
-import jsPDF from "jspdf";
-import "jspdf-autotable";
-
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import CircularProgress from "@mui/material/CircularProgress";
 
 import url from "../service/apiService";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const PokemonList = () => {
   interface Ability {
@@ -38,15 +38,16 @@ const PokemonList = () => {
     abilities: AbilityData[];
     image: string;
   }
-  const componentRef = useRef(null);
 
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState(0);
-  const [perPageOptions] = useState([10, 20, 40, 50, 100, 200]); // Opciones de cantidad por página
-  const [selectedPerPage, setSelectedPerPage] = useState(20); // Cantidad seleccionada por página
+  const [perPageOptions] = useState([10, 20, 40, 50, 100, 200]);
+  const [selectedPerPage, setSelectedPerPage] = useState(20);
   const [loading, setLoading] = useState<boolean>(false);
+  const [searchClicked, setSearchClicked] = useState<boolean>(false);
+  const [generatingPDF, setGeneratingPDF] = useState<boolean>(false);
 
   const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
@@ -56,13 +57,22 @@ const PokemonList = () => {
     setSearchTerm(event.target.value);
   };
 
-  const handlePerPageChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+  const handlePerPageChange = (
+    event: React.ChangeEvent<{ value: unknown }>
+  ) => {
     setSelectedPerPage(event.target.value as number);
-    setCurrentPage(1); // Reinicia la página al cambiar la cantidad por página
+    setCurrentPage(1);
   };
 
-  const getPokemos = async () => {
-    setLoading(true); // Mostrar indicador de progreso
+  const handleSearchClick = () => {
+    if (searchTerm.trim() !== "") {
+      getPokemons();
+      setSearchClicked(true);
+    }
+  };
+
+  const getPokemons = async () => {
+    setLoading(true);
 
     const response = await url
       .get(
@@ -73,7 +83,7 @@ const PokemonList = () => {
         console.error("Error fetching pokemons:", error);
       });
 
-    setLoading(false); // Ocultar indicador de progreso
+    setLoading(false);
 
     console.log(response?.data?.pokemonsList, "response");
     const pokemon = response?.data?.pokemonsList || [];
@@ -83,154 +93,203 @@ const PokemonList = () => {
     setTotalPages(totalPagesHeader ? parseInt(totalPagesHeader) : 0);
   };
 
-  useEffect(() => {
-    getPokemos();
-  }, [currentPage, searchTerm, selectedPerPage]); // Se ejecutará cada vez que currentPage, searchTerm o selectedPerPage cambien
+  const generatePDF = async () => {
+    setGeneratingPDF(true); // Cambiar el estado a true antes de comenzar la generación del PDF
 
-  useEffect(() => {
-    // Si searchTerm está vacío, recargar la vista
-    if (searchTerm === "") {
-      getPokemos();
-    }
-  }, [searchTerm]); // Se ejecutará cuando searchTerm cambie
+    const fetchSvgAsSvgString = async (url: string) => {
+      try {
+        const response = await fetch(url);
+        let svgText = await response.text();
+        svgText = svgText.replace(/<\?xml.*\?>/g, "");
+        svgText = svgText.replace(/^[^<]+/, "");
+        svgText = `${svgText}`;
 
-  // const handleDownloadPDF = () => {
-  //   const imgWidth = 100; // Ajusta el ancho de la imagen según tu preferencia
-  //   const imgHeight = 40; // Ajusta el alto de la imagen según tu preferencia
-  
-  //   // Set up the table
-  //   const columns = ["Nombre", "Imagen", "Habilidades"];
-  //   const rows = pokemons.map(({ name, image, abilities }) => [
-  //     name,
-  //     image, // Cambiando a solo la URL de la imagen
-  //     abilities.map(({ ability }) => ability.name).join(", "),
-  //   ]);
-  
-  //   // Add a new page for the table
-  //   const pdf = new jsPDF();
-  //   pdf.autoTable({
-  //     head: [columns],
-  //     body: rows,
-  //     columnStyles: {
-  //       1: { // La columna de las imágenes
-  //         cellWidth: imgWidth,
-  //         valign: 'middle',
-  //         halign: 'center',
-  //         // Utilizamos una función de renderizado personalizada para insertar la imagen
-  //         render: (cellData) => {
-  //           const img = new Image();
-  //           img.src = cellData.row.raw[1]; // Obtenemos la URL de la imagen
-  //           pdf.addImage(img, 'JPEG', cellData.cell.x, cellData.cell.y, imgWidth, imgHeight);
-  //         }
-  //       }
-  //     }
-  //   });
-  
-  //   // Save the PDF
-  //   pdf.save("pokemon_list.pdf");
-  // };
-  
-  const handleDownloadPDF = () => {
-    const imgWidth = 100; // Ajusta el ancho de la imagen según tu preferencia
-    const imgHeight = 40; // Ajusta el alto de la imagen según tu preferencia
-  
-    // Set up the table
-    const columns = ["Nombre", "Imagen", "Habilidades"];
-    const rows = pokemons.map(({ name, image, abilities }) => [
-      name,
-      { image: image, width: imgWidth, height: imgHeight }, // Renderiza la imagen
-      abilities.map(({ ability }) => ability.name).join(", "),
-    ]);
-  
-    // Add a new page for the table
-    const pdf = new jsPDF();
-    pdf.autoTable({
-      head: [columns],
-      body: rows,
-      columnStyles: {
-        1: { // La columna de las imágenes
-          cellWidth: imgWidth,
-          valign: 'middle',
-          halign: 'center',
-          // Utilizamos una función de renderizado personalizada para insertar la imagen
-          render: (cell) => {
-            const img = new Image();
-            img.src = cell.row.raw[1].image; // Accedemos a la URL de la imagen correctamente
-            pdf.addImage(img, 'JPEG', cell.cell.x, cell.cell.y, imgWidth, imgHeight);
-          }
-        }
+        return svgText;
+      } catch (error) {
+        console.error("Error:", error);
+        return null;
       }
-    });
-  
-    // Save the PDF
-    pdf.save("pokemon_list.pdf");
+    };
+    const updatedPokemons = await Promise.all(
+      pokemons.map(async (pokemon) => {
+        const svgString = await fetchSvgAsSvgString(pokemon.image);
+
+        return { ...pokemon, image: svgString };
+      })
+    );
+
+    const docDefinition = {
+      content: [
+        { text: "Lista de Pokemons", style: "title", alignment: "center" },
+        { text: "\n\n" },
+        {
+          style: "tableExample",
+          table: {
+            widths: ["auto", "auto", "*"],
+            body: [
+              [
+                { text: "Nombre", style: "tableHeader", alignment: "center" },
+                { text: "Imagen", style: "tableHeader", alignment: "center" },
+                { text: "Habilidad", style: "tableHeader", alignment: "center" },
+              ],
+
+              ...updatedPokemons.map((pokemon) => {
+                return [
+                  { text: pokemon.name, style: "subheader" },
+                  { svg: pokemon.image, width: 83 },
+                  {
+                    ul: pokemon.abilities.map((abilityData) => (
+                      `- Ability: ${abilityData.ability.name}, Hidden: ${
+                        abilityData.is_hidden ? "Yes" : "No"
+                      }`
+                    )),
+                  },
+                ];
+              }),
+            ],
+          },
+        },
+      ],
+      styles: {
+        title: {
+          fontSize: 25,
+          bold: true,
+          alignment: "center",
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15],
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 18,
+          color: "black",
+        },
+      },
+    };
+
+    pdfMake.createPdf(docDefinition).download("lista_pokemons.pdf");
+    setGeneratingPDF(false);
   };
-  
-  
+
+  useEffect(() => {
+    if (searchTerm === "" && searchClicked === true) {
+      getPokemons();
+      setSearchClicked(false);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    getPokemons();
+  }, [currentPage]);
+
+  useEffect(() => {
+    getPokemons();
+  }, [currentPage, selectedPerPage]);
+
   return (
     <Container maxWidth="lg">
-      {loading && <LinearProgress />} {/* Indicador de progreso */}
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          mt: 2,
-        }}
-      >
-        <Button variant="contained" color="primary" onClick={handleDownloadPDF}>
-          Descargar PDF
-        </Button>
-      </Box>
-      <div ref={componentRef}>
-      <Grid container spacing={2}>
-        {/* Sección del título y campo de búsqueda */}
-        <Grid item xs={12}>
-     
 
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography variant="h4">Lista de Pokemons</Typography>
+    <Grid container spacing={2}>
+    <Grid item xs={12}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <Typography variant="h4">Lista de Pokemons</Typography>
+        
+          <Box sx={{ display: "flex" }}>
             <TextField
               placeholder="buscar..."
               variant="outlined"
               value={searchTerm}
               onChange={handleSearchChange}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <SearchIcon />
-                  </InputAdornment>
-                ),
-              }}
             />
-          </Box>
-        </Grid>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSearchClick}
+              style={{ marginLeft: "10px" }} // Ajusta el espaciado entre el TextField y el botón
+            >
+              <SearchIcon />
+            </Button>
+            <Button
+            variant="contained"
+            color="primary"
+            onClick={generatePDF}
+            style={{ marginLeft: "10px" ,width:'50%'}} // Ajusta el espaciado entre el TextField y el botón
 
-        {/* Sección de los cards de Pokémon */}
-        <Grid item xs={12}>
+          >
+            {generatingPDF ? (
+              <CircularProgress
+                size={25}
+                style={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  marginTop: -12,
+                  marginLeft: -12,
+                  color: "white",
+                }}
+              />
+            ) : (
+              "Descargar PDF"
+            )}
+          </Button>
+          </Box>
+        </Box>
+     
+        {loading && <LinearProgress />}
+      </Grid>
+
+ 
+      <Grid item xs={12}>
+        {loading && (
           <Grid container spacing={2}>
-            {pokemons.map((pokemon, index) => (
+            {Array.from(Array(selectedPerPage)).map((_, index) => (
               <Grid item xs={12} sm={6} md={4} key={index}>
+                <Box
+                  sx={{
+                    width: 345,
+                    height: "100%",
+                    marginRight: 0.5,
+                    my: 5,
+                  }}
+                >
+                  <Skeleton variant="rectangular" width={345} height={130} />
+                  <Box sx={{ pr: 2, pt: 0.5 }}>
+                    <Skeleton />
+                    <Skeleton width="60%" />
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+
+        {/* Mostrar la lista de Pokémon cuando no está cargando */}
+        {!loading && (
+          <>
+            {pokemons.length === 1 && (
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                 <Card
                   sx={{
                     maxWidth: 345,
-                    height: "100%",
-                    border: "2px solid green",
+                    height: '100%',
+                    border: '2px solid green',
+                    marginTop: '20px', // Ajusta el margen según sea necesario
                   }}
                 >
                   <CardMedia
                     component="img"
-                    alt={pokemon.name}
+                    alt={pokemons[0].name}
                     height="130"
-                    sx={{ objectFit: "fill", marginTop: "10px" }}
-                    image={pokemon.image}
+                    sx={{ objectFit: "contain", marginTop: "10px" }}
+                    src={pokemons[0].image}
                   />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
-                      {pokemon.name}
+                      {pokemons[0].name}
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
                       <ul>
-                        {pokemon.abilities.map((abilityData, abilityIndex) => (
+                        {pokemons[0].abilities.map((abilityData, abilityIndex) => (
                           <li key={abilityIndex}>
                             Ability: {abilityData.ability.name}, Hidden:{" "}
                             {abilityData.is_hidden ? "Yes" : "No"}
@@ -240,44 +299,73 @@ const PokemonList = () => {
                     </Typography>
                   </CardContent>
                 </Card>
+              </Box>
+            )}
+
+            {pokemons.length > 1 && (
+              <Grid container spacing={2}>
+                {pokemons.map((pokemon, index) => (
+                  <Grid item xs={12} sm={6} md={4} key={index}>
+                    <Card
+                      sx={{
+                        maxWidth: 345,
+                        height: "100%",
+                        border: "2px solid green",
+                      }}
+                    >
+                      <CardMedia
+                        component="img"
+                        alt={pokemon.name}
+                        height="130"
+                        sx={{ objectFit: "contain", marginTop: "10px" }}
+                        src={pokemon.image}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {pokemon.name}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          <ul>
+                            {pokemon.abilities.map(
+                              (abilityData, abilityIndex) => (
+                                <li key={abilityIndex}>
+                                  Ability: {abilityData.ability.name}, Hidden:{" "}
+                                  {abilityData.is_hidden ? "Yes" : "No"}
+                                </li>
+                              )
+                            )}
+                          </ul>
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-            {/* Esqueleto para los Pokemons */}
-            {loading &&
-              Array.from(Array(selectedPerPage)).map((_, index) => (
-                <Grid item xs={12} sm={6} md={4} key={index}>
-                  <Box sx={{ width: 345, height: "100%", marginRight: 0.5, my: 5 }}>
-                    <Skeleton variant="rectangular" width={345} height={130} />
-                    <Box sx={{ pr: 2, pt: 0.5 }}>
-                      <Skeleton />
-                      <Skeleton width="60%" />
-                    </Box>
-                  </Box>
-                </Grid>
-              ))}
-          </Grid>
-        </Grid>
+            )}
+          </>
+        )}
       </Grid>
-      
-      {/* Sección de paginación y Select de cantidad por página */}
-      <Box
-        sx={{
-          position: "fixed",
-          bottom: 20,
-          left: 0,
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          bgcolor: "background.paper",
-          p: 2,
-        }}
-      >
-        <Pagination
-          count={totalPages}
-          page={currentPage}
-          onChange={handleChange}
-          color="primary"
-        />
+    </Grid>
+
+    <Box
+      sx={{
+        position: "fixed",
+        bottom: 20,
+        left: 0,
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        bgcolor: "background.paper",
+        p: 2,
+      }}
+    >
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={handleChange}
+        color="primary"
+      />
+      {pokemons.length !== 1 ? (
         <Select
           value={selectedPerPage}
           onChange={handlePerPageChange}
@@ -285,12 +373,16 @@ const PokemonList = () => {
           sx={{ ml: 2 }}
         >
           {perPageOptions.map((option) => (
-            <MenuItem key={option} value={option}>{option}</MenuItem>
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
           ))}
         </Select>
-      </Box>
-      </div >
-    </Container>
+      ) : null}
+    </Box>
+
+</Container>
+
   );
 };
 
--- poke-service/controller/pokemonController.js
+++ poke-service/controller/pokemonController.js
@@ -1,57 +1,84 @@
-// backend/controllers/pokemonController.js
 const axios = require('axios');
 
 const POKEAPI_BASE_URL = 'https://pokeapi.co/api/v2';
 
 exports.getPokemons = async (req, res) => {
-  try {
-    console.log(req.query,'query')
-    const { limit = 20, page = 1, search } = req.query;
-
-
-       // Verificar si los datos son paginables
-       if (isNaN(parseInt(limit)) || isNaN(parseInt(page))) {
+    try {
+      const { limit = 20, page = 1, search } = req.query;
+  
+      // Verificar si los datos son paginables
+      if (isNaN(parseInt(limit)) || isNaN(parseInt(page))) {
         return res.status(400).json({ message: 'Bad Request: Invalid limit or page value' });
       }
   
-
-    // Calculamos el offset para la paginación
-    const offset = (page - 1) * limit;
-    
-    // Construimos la URL de la solicitud a la PokeAPI
-    let url = `${POKEAPI_BASE_URL}/pokemon?offset=${offset}&limit=${limit}`;
-
-    // Agregamos el parámetro de búsqueda si está presente
-    if (search) {
-      url = `${POKEAPI_BASE_URL}/pokemon/${search}`;
-    }
-
-    // Realizamos la solicitud a la PokeAPI
-    const response = await axios.get(url);
-
-    // Obtenemos la lista de pokemons desde la respuesta
-    const pokemons = response.data.results;
-    const pokemonsList = await Promise.all(
-        pokemons.map(async (pokemon) => {
-          const pokemonResponse = await axios.get(pokemon.url);
-          return {
+      // Calculamos el offset para la paginación
+      const offset = (page - 1) * limit;
+      
+      // Construimos la URL de la solicitud a la PokeAPI
+      let url = `${POKEAPI_BASE_URL}/pokemon`;
+  
+      // Si se proporciona un parámetro de búsqueda (nombre o ID)
+      if (search) {
+        url += `/${search}`;
+      } else {
+        url += `?offset=${offset}&limit=${limit}`;
+      }
+  
+      // Realizamos la solicitud a la PokeAPI
+      const response = await axios.get(url);
+  
+      if (search) {
+        // Si se realizó una búsqueda por nombre o ID
+        const pokemon = response.data;
+        console.log(pokemon,'search');
+        if (!pokemon) {
+            return res.status(404).json({ message: 'Pokemon not found' });
+        }
+        const abilities =pokemon.abilities;
+        const image = pokemon.sprites.other.dream_world.front_default !== null ?pokemon.sprites.other.dream_world.front_default:'https://vignette.wikia.nocookie.net/pokemon-fano/images/6/6f/Poke_Ball.png/revision/latest?cb=20140520015336';
+  
+        // Devolvemos la respuesta estructurada
+        const pokemonsList = [{
             name: pokemon.name,
-            abilities: pokemonResponse.data.abilities,
-            image:pokemonResponse.data.sprites.other.dream_world.front_default  
-          };
-        })
-      );
+            abilities,
+            image,  
+        }]
+        res.json({
+            pokemonsList,
+          totalPages: 1,
+          currentPage: 1
+        });
+      } else {
+        // Si se obtuvo una lista de pokémons
+        const pokemons = response.data.results;
+        const pokemonsList = await Promise.all(
+          pokemons.map(async (pokemon) => {
+            const pokemonResponse = await axios.get(pokemon.url);
+            const abilities = pokemonResponse.data.abilities
+            const image = pokemonResponse.data.sprites.other.dream_world.front_default !== null ?pokemonResponse.data.sprites.other.dream_world.front_default:'https://vignette.wikia.nocookie.net/pokemon-fano/images/6/6f/Poke_Ball.png/revision/latest?cb=20140520015336';
   
-    // Ordenamos alfabéticamente los pokemons
-    pokemonsList.sort((a, b) => a.name.localeCompare(b.name));
-
-    const totalPokemons = response.data.count;
-    const totalPages = Math.ceil(totalPokemons / limit);
+            return {
+              name: pokemon.name,
+              abilities,
+              image
+            };
+          })
+        );
   
-   // Devolvemos la lista de pokemons ordenada como respuesta
-    res.json({pokemonsList,totalPages, currentPage: parseInt(page)});
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-};
+        // Ordenamos alfabéticamente los pokémons
+        pokemonsList.sort((a, b) => a.name.localeCompare(b.name));
+  
+        const totalPokemons = response.data.count;
+        const totalPages = Math.ceil(totalPokemons / limit);
+    
+        // Devolvemos la misma estructura de respuesta para la lista de pokémons
+        res.json({ pokemonsList, totalPages, currentPage: parseInt(page) });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  };
+  
+  
+  
